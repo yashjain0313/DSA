295092c9743c2a92e897fbfc0c4f209e3650a8bd
Yash Jain <yashjain0313@gmail.com>
2025-12-04 23:58:42 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (90.18%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* temp = curr->next; // Store the next node
+            curr->next = prev;            // Reverse the current node's pointer
+            prev = curr;                  // Move prev to current node
+            curr = temp;                  // Move to the next node
+        }
+        return prev; // New head of the reversed list
+    }
+};
