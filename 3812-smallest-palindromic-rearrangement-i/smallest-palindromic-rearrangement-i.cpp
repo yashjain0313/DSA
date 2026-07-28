40c96c6fa23e6e6a862566907072661583cc0da3
Yash Jain <yashjain0313@gmail.com>
2026-07-28 16:58:42 +0530

    Time: 177 ms (9.89%) | Memory: 81.8 MB (7.91%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+    map<char, int> freq;
+
+for(char c : s)
+    freq[c]++;
+
+string left = "", mid = "";
+
+for(auto it = freq.begin(); it != freq.end(); ++it) {
+    left += string(it->second / 2, it->first);
+
+    if(it->second % 2 == 1 && mid.empty())
+        mid = it->first;
+}
+
+string right = left;
+reverse(right.begin(), right.end());
+
+return left + mid + right;
+        
+    }
+};
