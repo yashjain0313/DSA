b7563768f0ff3e3b02ee66ab45c806586aaad74d
Yash Jain <yashjain0313@gmail.com>
2026-08-24 12:36:27 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (14.88%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       int m = 1;
-       int s = nums[0];
- cout<<s<<endl;
-       for(int i =1 ; i<nums.size();i++)
-       {
-        if(nums[i]==s) {
-            
-            m++;
+        map<int,int>mp;
+        int n = nums.size()/2;
+        int a = 0 ;
+        for(auto i : nums)
+        {
+            mp[i]++;
+            if(mp[i]>n){
+                a=i;
+                break;
+            }
         }
-//boyer moore 
-        else{
-            m--;
-            if(m==0)
-            {
-                m=1;
-                s=nums[i];
-            }
-            }
-
-            cout<<s<<endl;
-       }
-
-       return s;
+return a;
     }
 };
